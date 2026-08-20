15fe0323eefe53035fdb750ffc2d4f9d70ee7d5e
PUSHKAR <pushkarrajishwar1@gmail.com>
2026-08-20 21:37:49 +0530

    Create Basic Structure of a Java Program.java

---
+++ WEEK 1/Basic Structure of a Java Program.java
@@ -0,0 +1,15 @@
+public class HelloDetails {
+    public static void main(String[] args) {
+        // Declare and initialize personal/college details
+        String name = "Your Name";
+        String rollNumber = "21XX1A0501";
+        String department = "Computer Science and Engineering";
+        String college = "XYZ College of Engineering";
+ 
+        // Display the details on the console
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Department: " + department);
+        System.out.println("College: " + college);
+    }
+}
