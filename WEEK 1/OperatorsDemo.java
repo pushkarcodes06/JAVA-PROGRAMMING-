41f4af2100b00ca478c8f6e6895cb9e993b2cd6e
PUSHKAR <pushkarrajishwar1@gmail.com>
2026-08-20 21:48:02 +0530

    Update OperatorsDemo.java

--- WEEK 1/OperatorsDemo.java
+++ WEEK 1/OperatorsDemo.java
@@ -19,7 +19,13 @@ public class OperatorsDemo {
         System.out.println("a % b = " + (a % b));
  
         // ---------- Relational Operators ----------
-      
+        System.out.println("\n--- Relational Operators ---");
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
  
         // ---------- Logical Operators ----------
         System.out.println("\n--- Logical Operators ---");
