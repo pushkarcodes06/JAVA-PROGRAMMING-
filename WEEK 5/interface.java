3668408124fcc385e5ca623907add3fb365e110d
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-08-26 22:03:55 +0530

    Create interface.java

---
+++ WEEK 5/interface.java
@@ -0,0 +1,13 @@
+interface Flyable { void fly(); }
+interface Swimmable { void swim(); }
+class Duck implements Flyable, Swimmable {
+	public void fly()  { System.out.println("Duck flies"); }
+	public void swim() { System.out.println("Duck swims"); }
+}
+public class Test {
+	public static void main(String[] args) {
+    	Duck d = new Duck();
+    	d.fly();
+    	d.swim();
+	}
+}
